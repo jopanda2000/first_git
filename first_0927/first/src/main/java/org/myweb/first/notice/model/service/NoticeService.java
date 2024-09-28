9e41e438ab581f8edf4dcb9d898b84ceb6055fed
jopanda2000 <jochihyun@kw.ac.kr>
2024-09-28 11:40:23 +0900

    Modify file

---
+++ first_0927/first/src/main/java/org/myweb/first/notice/model/service/NoticeService.java
@@ -0,0 +1,18 @@
+package org.myweb.first.notice.model.service;
+
+import java.util.ArrayList;
+
+import org.myweb.first.notice.model.dto.Notice;
+
+public interface NoticeService {
+	Notice selectLast();
+	ArrayList<Notice> selectTop3();
+	Notice selectNotice(int noticeNo);
+	ArrayList<Notice> selectList();
+	
+	ArrayList<Notice> selectSearchTitle(String keyword);
+	
+	int insertNotice(Notice notice);
+	int updateAddReadCount(int noticeNo);
+	
+}
